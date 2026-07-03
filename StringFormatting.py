e6587c61c2ba50aef12f4c277fe8bbc2a23efa90
Ahmadhumayoun <81822752+Ahmadhumayoun@users.noreply.github.com>
2026-07-03 17:48:17 +0500

    m

---
+++ StringFormatting.py
@@ -0,0 +1,12 @@
+firstName=  input("first name: ")
+lastName= input("last name: ")
+age = input("age: ")
+gender = input("Gender: ")
+lDegree = input("last Degree: ")
+lExp = input("last Exp: ")
+print(f" Welcome {firstName} {lastName}! \n"
+      f"======== we have all your data=========\n"
+      f"Your age is {age} years old\n"
+      f"your gender is {gender}\n"
+      f"your last degree is {lDegree}\n"
+      f"&& you exp is {lExp}")
